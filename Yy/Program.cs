944890e50ce3bf3656e2234c4e2ee2c594173101
Alena <podlesnykh.a@mail.ru>
2024-02-05 14:47:46 +0300

    This is a solution to two tasks

--- Yy/Program.cs
+++ Yy/Program.cs
@@ -1,2 +1,36 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Задача первая:
+
+// int a = 563;
+
+// if (a % 7 == 0 && a % 23 == 0)
+// {
+// Console.WriteLine($"Число {a} кратно");
+// }
+// else
+//  {
+// Console.WriteLine($"Число {a} не кратно");
+// }
+
+
+
+ //Задача вторая
+ 
+
+int x = 3;
+int y = -10;
+if (x > 0 && y > 0)
+{
+    Console.WriteLine($"Это первая четверть");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine($"Это вторая четверть");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine($"Это третья четверть");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine($"Это четвертая четверть");
+}
