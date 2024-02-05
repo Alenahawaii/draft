c549bb74f8492d0e4c4073bf132f98d637d654ea
Alena <podlesnykh.a@mail.ru>
2024-02-05 15:09:37 +0300

    Task3

--- Yy/Program.cs
+++ Yy/Program.cs
@@ -14,23 +14,37 @@
 
 
  //Задача вторая
- 
 
-int x = 3;
-int y = -10;
-if (x > 0 && y > 0)
+
+// int x = 3;
+// int y = -10;
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine($"Это первая четверть");
+// }
+// if (x < 0 && y > 0)
+// {
+//     Console.WriteLine($"Это вторая четверть");
+// }
+// if (x < 0 && y < 0)
+// {
+//     Console.WriteLine($"Это третья четверть");
+// }
+// if (x > 0 && y < 0)
+// {
+//     Console.WriteLine($"Это четвертая четверть");
+// }
+
+int number = 39;
+int firstDigit = (number / 10);
+int secondDigit = (number % 10);
+if (number >= 10 && number <= 99)
+
+if (firstDigit < secondDigit)
 {
-    Console.WriteLine($"Это первая четверть");
+    Console.WriteLine($"Вторая цифра больше  - {secondDigit}");
 }
-if (x < 0 && y > 0)
+if (firstDigit > secondDigit)
 {
-    Console.WriteLine($"Это вторая четверть");
-}
-if (x < 0 && y < 0)
-{
-    Console.WriteLine($"Это третья четверть");
-}
-if (x > 0 && y < 0)
-{
-    Console.WriteLine($"Это четвертая четверть");
+    Console.WriteLine($"Первая цифра больше  - {firstDigit}");
 }
